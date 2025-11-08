74e7f9fffdf29b018e6c0f57a285c32967b58b08
Huynh Thi Ngoc Anh <huynhthingocanh2008@gmail.com>
2025-11-08 07:43:18 +0700

    Cập nhật code mới

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -15,7 +15,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('news:import')->dailyAt('08:00');
+        $schedule->command('news:import')->dailyAt('07:00');
     }
 
     /**
